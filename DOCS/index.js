91b0a7b90ed6da9d7653f1c013db5d71cc7f623f
luiscetta <filipe.cetta@gmail.com>
2021-10-21 21:23:44 -0300

    first commit

---
+++ DOCS/index.js
@@ -0,0 +1,43 @@
+const linksSocialMedia = {
+    github: "luiscetta",
+    youtube: "channel/UChCUlC4pzV0B439bECe_IjA",
+    facebook: "luiscetta",
+    instagram: "cetta_lojinha",
+    twitter: "_cettou",
+};
+
+function changeSocialMediaLinks() {
+    // acessando a dom pelo id de outra forma
+    // userName.textContent = "Luis Filipe Cetta"
+
+    // fazendo o for pegando a tag filha do elemento pai socialLinks.
+    for (let li of socialLinks.children) {
+        // pegando o atributo class da tag li.
+        const social = li.getAttribute('class');
+
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
+    };
+}
+
+changeSocialMediaLinks();
+
+function getGithubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    // pegar na API 
+    fetch(url)
+
+        // transformando em JSON
+        .then(res => res.json())
+
+        // o data armazena o res já em JSON dentro dela.
+        .then(data => { 
+            userName.textContent = data.name;
+            userBio.textContent = data.bio;
+            userProfile.href = data.html_url;
+            userImage.src = data.avatar_url;
+            userLogin.textContent = data.login;
+        });
+}
+
+getGithubProfileInfos();
---
+++ passwords/senhas.md
@@ -0,0 +1,3 @@
+#RumoAoPróximoNível
+#Protagonistas
+#ImaginarConstruirTransformar
